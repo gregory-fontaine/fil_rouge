a1dfe37f6db5aced390c51199a81f23fbca8a6dd
gregory fontaine <115070671+gregory-fontaine@users.noreply.github.com>
2023-03-12 16:02:03 +0100

    recup

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -34,7 +34,7 @@ class RegistrationController extends AbstractController
             $entityManager->flush();
             // do anything else you need here, like send an email
 
-            return $this->redirectToRoute('home.index');
+            return $this->redirectToRoute('app_ingredient_index');
         }
 
         return $this->render('registration/register.html.twig', [
